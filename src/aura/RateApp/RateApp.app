a1ec1ceb0762c846ed59f76262eb06c37ad47f7f
Dragomir Rangelov <dragovd4@gmail.com>
2018-10-15 14:33:51 +0300

    added lightning component for rating a Trial

---
+++ src/aura/RateApp/RateApp.app
@@ -0,0 +1,8 @@
+<!--
+ - Created by drang on 10/11/2018.
+ -->
+
+<aura:application description="rateApp" extends="force:slds">
+
+    <c:RateTrial/>
+</aura:application>
---
+++ src/aura/RateTrial/RateTrialHelper.js
@@ -0,0 +1,30 @@
+({
+
+    /**
+        *   @author: Dragomir Rangelov
+            @date: 11-10-2018
+            @description: Queue up a remote method call to saveRating Apex controller to create a record
+            @param:
+            @returns:
+        **/
+
+    createRating: function(component, newRating) {
+        var action = component.get("c.saveRating");
+
+        action.setParams({
+            "rating": newRating
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+
+            if (state === "SUCCESS") {
+
+                alert("Your rating has been submitted!");
+
+            }
+        });
+        $A.enqueueAction(action);
+
+    },
+
+})
